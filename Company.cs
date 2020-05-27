7a954a972ba19d7d6354c6480e6d5b0cae43a654
angrynerds-marcin <50827890+angrynerds-marcin@users.noreply.github.com>
2020-05-27 20:42:12 +0200

    Create Company.cs

---
+++ Company.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApplication8.NewFolder
+{
+    public class Company
+    {
+        public DateTime CreatedAt { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
